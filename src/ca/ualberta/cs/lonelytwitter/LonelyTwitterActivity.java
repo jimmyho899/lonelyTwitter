20d84b7012eaeb298ceb5033c13477d13d0e9d6a
jimmyho899 <jph@ualberta.ca>
2015-01-21 16:26:38 -0700

    Fixed the code.

--- src/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ src/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -6,8 +6,13 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Date;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import android.app.Activity;
 import android.content.Context;
@@ -55,21 +60,22 @@ public class LonelyTwitterActivity extends Activity {
 		
 		super.onStart();
 		tweets = loadFromFile();
-		//adapter = new ArrayAdapter<String>(this, R.layout.list_item, tweets);
+		adapter = new ArrayAdapter<String>(this, R.layout.list_item, tweets);
 		oldTweetsList.setAdapter(adapter);
 	}
 
 	private ArrayList<String> loadFromFile() {
-		ArrayList<String> tweets = new ArrayList<String>();
+		ArrayList<String> tweets = null;
+		Gson gson = new Gson();
+		
 		try {
 			FileInputStream fis = openFileInput(FILENAME);
-			BufferedReader in = new BufferedReader(new InputStreamReader(fis));
-			String line = in.readLine();
-			while (line != null) {
-				tweets.add(line);
-				line = in.readLine();
-			}
-
+			InputStreamReader isr = new InputStreamReader(fis);
+			// https://sites.google.com/site/gson/gson-user-guide 2015-01-21
+			Type dataType = new TypeToken<ArrayList<String>>() {}.getType();
+			tweets = gson.fromJson(isr, dataType);
+			fis.close();
+			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -77,15 +83,20 @@ public class LonelyTwitterActivity extends Activity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		if (tweets==null) { 
+			tweets = new ArrayList<String>();
+		}
 		return tweets;
 	}
 	
 	private void saveInFile(String text, Date date) {
+		Gson gson = new Gson();
+		
 		try {
-			FileOutputStream fos = openFileOutput(FILENAME,
-					Context.MODE_APPEND);
-			fos.write(new String(date.toString() + " | " + text)
-					.getBytes());
+			FileOutputStream fos = openFileOutput(FILENAME, 0);
+			OutputStreamWriter osw = new OutputStreamWriter(fos);
+			gson.toJson(tweets, osw);
+			osw.flush();
 			fos.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
